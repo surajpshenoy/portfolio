fe0764a0d3b1844d3f0877fd9ff5f8d2cb05ae89
surajpshenoy <surajpshenoy@gmail.com>
2021-09-17 17:45:26 +1000

    Right container added -& fixed colour issue in topbar

--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -1,26 +1,31 @@
 import React from "react";
 import "./Topbar.scss";
-import { Person, Mail } from "@material-ui/icons";
+import { GitHub, Mail } from "@material-ui/icons";
 
 export default function Topbar() {
   return (
-    <div className="topbar">
-      <div className="main">
-        <div className="details">
+    <div className="topbar active">
+      <div className="mainWrapper">
+        <div className="leftContainer">
           <a href="#intro" className="logo">
             Suraj Shenoy
           </a>
           <div className="infoContainer">
-            <Person className="icon" />
-            <span>+610000000</span>
+            <GitHub className="icon" />
+            <span>/surajpshenoy</span>
           </div>
           <div className="infoContainer">
             <Mail className="icon" />
             <span>surajpshenoy@gmail.com</span>
           </div>
         </div>
-        <div></div>
-        <div className="rigth-btn">right btn</div>
+        <div className="rightContainer">
+          <div className="hamburgerBtn">
+            <span className="bar1"></span>
+            <span className="bar2"></span>
+            <span className="bar3"></span>
+          </div>
+        </div>
       </div>
     </div>
   );
