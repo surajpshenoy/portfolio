d03972515cc2b66a712879edb7efb94f98da1648
surajpshenoy <surajpshenoy@gmail.com>
2021-09-17 16:42:22 +1000

    Topbar leftinfo

--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import "./Topbar.scss"
+import {Person, Mail} from "@material-ui/icons"
 
 export default function Topbar() {
     return (
         <div className="topbar">
-            <div>
-                <div>
-                    <a href="#intro">Suraj Shenoy</a>
+            <div className="main">
+                <div className="details">
+                    <a href="#intro" className="logo">Suraj Shenoy</a>
+                    <div className="infoContainer">
+                    <Person className="icon"/><span>+61435585759</span>
+                    </div>
+                    <div className="infoContainer">
+                    <Mail className="icon"/>
+                    <span>surajpshenoy@gmail.com</span>
+                    </div>
                 </div>
+                <div>
+                    
+                </div>
+                <div className="rigth-btn">
+                    right btn
+                 </div>
             </div>
         </div>
     )
