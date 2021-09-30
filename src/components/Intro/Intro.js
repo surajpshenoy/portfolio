508ad37e611207f600c72d9cb02c7da78ab1ccfb
surajpshenoy <surajpshenoy@gmail.com>
2021-09-30 15:44:25 +1000

    Right intro added

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Intro.scss";
+//import { KeyboardArrowDown } from "@material-ui/icons"
 export default function Intro() {
   return (
     <div className="intro" id="intro">
@@ -10,8 +11,13 @@ export default function Intro() {
       </div>
       <div className="right">
         <div className="wrapper">
-          <h2></h2>
+          <h2>Hi There. I am</h2>
+          <h1>Suraj Shenoy</h1>
+          <h3>Web <span>Developer</span></h3>
         </div>
+        <a href="#work">
+          <img src="assets/arrow2.png" alt="arrow-down"></img>
+        </a>
       </div>
     </div>
   );
