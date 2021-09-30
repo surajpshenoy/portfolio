71956ce181c8214bbb54dd59119dc8ec69673beb
surajpshenoy <surajpshenoy@gmail.com>
2021-09-30 15:29:39 +1000

    Added the intro left container

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Contact from "./components/Contact/Contact";
 import Topbar from "./components/Topbar/Topbar";
 import Intro from "./components/Intro/Intro";
 import Project from "./components/Projects/Project";
 import Work from "./components/Work/Work";
-import Menu from "./components/Menu/Menu"
+import Menu from "./components/Menu/Menu";
 import "./App.scss";
 
-
 function App() {
-  const [menuOpen, setMenuOpen] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="app">
-      <Topbar menuOpen= {menuOpen} setMenuOpen={setMenuOpen} />
-      <Menu menuOpen= {menuOpen} setMenuOpen={setMenuOpen}/>
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <Intro />
         <Work />
--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,5 +1,18 @@
 import React from "react";
 import "./Intro.scss";
 export default function Intro() {
-  return <div className="intro" id="intro"></div>;
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/ss2.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2></h2>
+        </div>
+      </div>
+    </div>
+  );
 }
