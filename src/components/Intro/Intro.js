67a4538707ed1ffb96203539dece0b1bea2e2f2a
surajpshenoy <surajpshenoy@gmail.com>
2021-10-01 13:55:15 +1000

    Added the animation for right intro container

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./Intro.scss";
+import { init } from "ityped";
 //import { KeyboardArrowDown } from "@material-ui/icons"
 export default function Intro() {
+  const textRef = useRef();
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backSpeed:  60,
+      backDelay: 1500,
+      strings: ["Developer", "Tester", "Designer" ],
+    });
+  }, []);
+
   return (
     <div className="intro" id="intro">
       <div className="left">
         <div className="imgContainer">
-          <img src="assets/ss2.png" alt="" />
+          <img src="assets/me.png" alt="" />
         </div>
       </div>
       <div className="right">
         <div className="wrapper">
-          <h2>Hi There. I am</h2>
+          <h2>Hi there, I am</h2>
           <h1>Suraj Shenoy</h1>
-          <h3>Web <span>Developer</span></h3>
+          <h3>
+            Web Application <span ref={textRef}></span>
+          </h3>
         </div>
         <a href="#work">
           <img src="assets/arrow2.png" alt="arrow-down"></img>
